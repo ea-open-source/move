8e43cc49f51f7d1dc7b74f6c92c7ddeb1d42edbb
ea-open-source <open-source@eastagile.com>
2022-05-30 12:18:09 +0700

    Refactor test code

--- language/tools/move-cli/tests/cli_tests.rs
+++ language/tools/move-cli/tests/cli_tests.rs
@@ -1,16 +1,18 @@
 // Copyright (c) The Diem Core Contributors
 // SPDX-License-Identifier: Apache-2.0
 
-use std::{env, fs};
-use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
 use move_cli::sandbox::commands::test;
+#[cfg(unix)]
+use std::fs::File;
+use std::io::Write;
+use std::{env, fs};
 
+use home::home_dir;
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 use std::process::Stdio;
-use home::home_dir;
 use toml_edit::easy::Value;
-use std::os::unix::fs::PermissionsExt;
 
 pub const CLI_METATEST_PATH: [&str; 3] = ["tests", "metatests", "args.txt"];
 
@@ -72,59 +74,88 @@ fn save_credential_works() {
     const CLI_EXE: &str = "../../../target/debug/move";
     #[cfg(not(debug_assertions))]
     const CLI_EXE: &str = "../../../target/release/move";
-    let home = home_dir().unwrap().to_string_lossy().to_string() + "/.move/test";
-    env::set_var("MOVE_HOME", &home);
-    match std::process::Command::new(CLI_EXE)
-        .current_dir(".")
-        .args(["login"])
-        .stdin(Stdio::piped()).spawn() {
-            Ok(mut child) => {
-                let token = "test_token";
-                child.stdin.as_ref().unwrap().write(token.as_bytes()).unwrap();
-                child.wait().unwrap();
-                Ok(())
-            }
-            Err(error) => Err(error),
-        }.unwrap();
-    let contents = fs::read_to_string(home + "/credential.toml").expect("Unable to read file");
-    let mut toml: Value = contents.parse().unwrap();
-    let registry = toml.as_table_mut().unwrap().get_mut("registry").unwrap();
-    let token = registry.as_table_mut().unwrap().get_mut("token").unwrap();
-    assert!(token.to_string().contains("test_token"));
-}
+    let (move_home, credential_path) = setup_move_home();
+    assert!(fs::read_to_string(&credential_path).is_err());
 
-#[test]
-fn save_credential_fails() {
-    #[cfg(debug_assertions)]
-    const CLI_EXE: &str = "../../../target/debug/move";
-    #[cfg(not(debug_assertions))]
-    const CLI_EXE: &str = "../../../target/release/move";
-    let home = home_dir().unwrap().to_string_lossy().to_string() + "/.move/test";
-    let credential_file = home.clone() + "/credential.toml";
-    let _ = fs::remove_file(&credential_file);
-
-    fs::create_dir_all(&home).unwrap();
-    let file = File::create(&credential_file).unwrap();
-    let mut perms = file.metadata().unwrap().permissions();
-    perms.set_mode(0o000);
-    file.set_permissions(perms).unwrap();
-
-    env::set_var("MOVE_HOME", &home);
     match std::process::Command::new(CLI_EXE)
         .current_dir(".")
         .args(["login"])
         .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Ok(child) => {
+            let token = "test_token";
+            child
+                .stdin
+                .as_ref()
+                .unwrap()
+                .write(token.as_bytes())
+                .unwrap();
+            match child.wait_with_output() {
+                Ok(output) => {
+                    assert!(String::from_utf8_lossy(&output.stdout).contains(
+                        "Please paste the API Token found on \
+                                https://movey-app-staging.herokuapp.com/settings/tokens below"
+                    ));
+                    Ok(())
+                }
+                Err(error) => Err(error),
+            }
+        }
+        Err(error) => Err(error),
+    }
+    .unwrap();
+
+    let contents = fs::read_to_string(&credential_path).expect("Unable to read file");
+    let mut toml: Value = contents.parse().unwrap();
+    let registry = toml.as_table_mut().unwrap().get_mut("registry").unwrap();
+    let token = registry.as_table_mut().unwrap().get_mut("token").unwrap();
+    assert!(token.to_string().contains("test_token"));
+
+    clean_up(&move_home)
+}
+
+#[cfg(unix)]
+#[test]
+fn save_credential_fails_if_undeletable_credential_file_exists() {
+    #[cfg(debug_assertions)]
+    const CLI_EXE: &str = "../../../target/debug/move";
+    #[cfg(not(debug_assertions))]
+    const CLI_EXE: &str = "../../../target/release/move";
+    let (move_home, credential_path) = setup_move_home();
+    let file = File::create(&credential_path).unwrap();
+    let mut perms = file.metadata().unwrap().permissions();
+    perms.set_mode(0o000);
+    file.set_permissions(perms).unwrap();
+
+    match std::process::Command::new(CLI_EXE)
+        .current_dir(".")
+        .args(["login"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn() {
-        Ok(mut child) => {
+        Ok(child) => {
             let token = "test_token";
             child.stdin.as_ref().unwrap().write(token.as_bytes()).unwrap();
-            child.wait().unwrap();
-            let out = BufReader::new(child.stderr.take().unwrap());
-            out.lines().for_each(|line|
-                assert!(line.unwrap().contains("Error: Error reading input: Permission denied (os error 13)"))
-            );
-            Ok(())
+            match child.wait_with_output() {
+                Ok(output) => {
+                    assert!(String::from_utf8_lossy(&output.stdout)
+                        .contains(
+                            "Please paste the API Token found on \
+                                    https://movey-app-staging.herokuapp.com/settings/tokens below"
+                        )
+                    );
+                    assert!(String::from_utf8_lossy(&output.stderr)
+                        .contains(
+                            "Error: Error reading input: Permission denied (os error 13)"
+                        )
+                    );
+                    Ok(())
+                },
+                Err(error) => Err(error),
+            }
         }
         Err(error) => Err(error),
     }.unwrap();
@@ -132,5 +163,20 @@ fn save_credential_fails() {
     let mut perms = file.metadata().unwrap().permissions();
     perms.set_mode(0o600);
     file.set_permissions(perms).unwrap();
-    let _ = fs::remove_file(&credential_file);
+    let _ = fs::remove_file(&credential_path);
+
+    clean_up(&move_home)
+}
+
+fn setup_move_home() -> (String, String) {
+    let move_home = home_dir().unwrap().to_string_lossy().to_string() + "/.move/test";
+    env::set_var("MOVE_HOME", &move_home);
+    let _ = fs::remove_dir_all(&move_home);
+    fs::create_dir_all(&move_home).unwrap();
+    let credential_path = move_home.clone() + "/credential.toml";
+    return (move_home, credential_path);
+}
+
+fn clean_up(move_home: &str) {
+    let _ = fs::remove_dir_all(move_home);
 }
